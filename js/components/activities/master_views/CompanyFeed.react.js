841a294014a805b94ff1d297855ae1888b6f77cd
Askhat Omarov <askhat.omarov91@gmail.com>
2014-12-17 20:00:59 +0600

    company feed page

--- js/components/activities/master_views/CompanyFeed.react.js
+++ js/components/activities/master_views/CompanyFeed.react.js
@@ -13,7 +13,13 @@ var ActiveState = Router.ActiveState;
 var Link = Router.Link;
 var IconSvg = require('../../common/IconSvg.react');
 var ActivityStore = require('../../../stores/ActivityStore');
+var ContactStore = require('../../../stores/ContactStore');
+var SalesCycleStore = require('../../../stores/SalesCycleStore');
+var UserStore = require('../../../stores/UserStore');
 var AppContextMixin = require('../../../mixins/AppContextMixin');
+var Form = require('../../../forms/Form.react');
+var inputs = require('../../../forms/input');
+var Input = inputs.Input;
 
 function get_activity_number() {
     return _.size(ActivityStore.getByDate());
@@ -73,21 +79,196 @@ var CompanyFeedLink = React.createClass({
     }
 });
 
+var FilterBar = React.createClass({
+    propTypes: {
+        value: React.PropTypes.object,
+        onHandleUserInput: React.PropTypes.func
+    },
+    render: function() {
+        return (
+            <Form onUpdate={this.onHandleUpdate} value={this.props.value} name='activity:filter_activity_form' ref='filter_activity_form'>
+                <div className="page-header-filterContainer">
+                    <div className="page-header-filter row">
+                        <div className="row-icon">
+                            <IconSvg iconKey='search' />
+                        </div>
+                        <div className="row-body row-body--inverted">
+                            <div className="row-body-secondary">
+                                <IconSvg iconKey='arrow-down' />
+                            </div>
+                            <div className="row-body-primary">
+                                <Input name="filter_text" type="text" className="input-filter" placeholder="Фильтр" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Form>
+        )
+    },
+    onHandleUpdate: function(value) {
+        var form = this.refs.filter_activity_form;
+        var errors = form.validate();
+        if(!errors) {
+            this.props.onHandleUserInput(form.value());
+        } else {
+            alert(errors);
+        }
+    }
+
+});
+
+var ActivityListItem = React.createClass({
+    mixins: [AppContextMixin],
+    propTypes: {
+        activity: React.PropTypes.object,
+    },
+
+    getAuthor: function(user_id) {
+        return UserStore.get(user_id);
+    },
+
+    getContact: function(sc_id) {
+        return ContactStore.get(SalesCycleStore.get(sc_id).contact_id);
+    },
+
+    render: function() {
+        var activity = this.props.activity;
+        var author = this.getAuthor(activity.author_id);
+        var contact = this.getContact(activity.salescycle_id);
+        return (
+            <div className="stream-item">
+                <div className="row">
+                    <div className="row-icon">
+                        <IconSvg iconKey={activity.feedback} />
+                  </div>
+                  <div className="row-body row-body--no-trailer">
+                    <div className="row">
+                      <a href="#" className="row-icon">
+                        <figure className="icon-userpic">
+                            <img src={"img/userpics/" + author.userpic} />
+                        </figure>
+                      </a>
+                      <div className="row-body">
+                        <div className="row">
+                          <div className="row-body-primary text-caption text-secondary">
+                            <a href="#" className="text-secondary">{author.first_name} {author.last_name}</a> в {activity.at}
+                          </div>
+                          <div className="row-body-secondary">
+                            <a href="#" className="row-icon">
+                                <IconSvg iconKey="comment" />
+                            </a>
+                          </div>
+                        </div>
+                        <div className="row-body-message">
+                            {activity.description}
+                        </div>
+                        <ul className="stream-breadcrumbs">
+                            <li>
+                                <Link to='contact_profile' params={{id: contact.id}} className="stream-breadcrumbs">{contact.fn}</Link>
+                            </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+        )
+    }
+});
+
+var ActivityList = React.createClass({
+    propTypes: {
+        filter_text: React.PropTypes.string,
+        activities: React.PropTypes.array,
+        selection_map: React.PropTypes.object,
+        onChangeState: React.PropTypes.func
+    },
+
+    render: function() {
+        var activityListItems = activities.map(function(activity) {
+            return(
+                <ActivityListItem
+                    activity={activity} />
+            )
+        }.bind(this));
+
+        return (
+            <div className="page-body">
+                {activityListItems}
+            </div>
+        )
+    }
+});
+
 var CompanyFeedDetailView = React.createClass({
     mixins: [Router.Navigation, AppContextMixin],
     propTypes: {
         label: React.PropTypes.string
+    },
+
+    getInitialState: function() {
+        activities = ActivityStore.getByDate(true);
+        return {
+            activities: activities,
+            search_bar: {select_all: false, filter_text: ''}
+        }
+    },
+
+    getFilterText: function() {
+        return this.state.search_bar.filter_text;
+    },
+
+    getActivities: function() {
+        return this.state.activities;
+    },
+
+    onFilterBarUpdate: function(value) {
+        var _map = {}, changed = value.select_all ^ this.state.search_bar.select_all,
+            activities = null;
+        if(value.filter_text) {
+            activities = fuzzySearch(this.state.activities, value.filter_text, {
+                'keys': ['description']});
+        } else {
+            activities = ActivityStore.getByDate(true);
+        }
+        for(var activity_id in this.state.selection_map) {
+            _map[activity_id] = false;
+        }
+        for(var i = 0; i<activities.length; i++) {
+            activity_id = activities[i].id;
+            if(changed) {
+                _map[activity_id] = value.select_all;
+            } else if(value.select_all) {
+                _map[activity_id] = true;
+            } else {
+                _map[activity_id] = this.state.selection_map[activity_id];
+            }
+        }
+
+        var newState = React.addons.update(this.state, {
+            activities: {$set: activities},
+            selection_map: {$set: _map},
+            search_bar: {$set: value},
+        });
+        this.setState(newState);
     },
     
     render: function() {
         return (
             <div className="page page--noHeaderOpts">
                 <div className="page-header">
-                    <h1>CompanyFeed</h1>
+                    <ul className="page-breadcrumbs">
+        <li><a href="#" className="page-breadcrumbs-link">Моя лента</a></li>
+      </ul>
+                    <FilterBar
+                        ref="filter_bar"
+                        value={this.state.search_bar}
+                        onHandleUserInput={this.onFilterBarUpdate} />
                 </div>
-                <div className="page-body">
-                    <h4>CompanyFeed</h4>
-                </div>
+                <ActivityList
+                    ref="activity_list"
+                    filter_text={this.getFilterText()}
+                    activities={this.getActivities()} />
             </div>
         )
     },
