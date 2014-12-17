379104ae259eaa15f4d63e1e474df16af93219e3
Askhat Omarov <askhat.omarov91@gmail.com>
2014-12-17 23:26:02 +0600

    reusable ActivityList

--- js/components/activities/master_views/MyFeed.react.js
+++ js/components/activities/master_views/MyFeed.react.js
@@ -24,6 +24,7 @@ var SVGCheckbox = inputs.SVGCheckbox;
 var Input = inputs.Input;
 var Div = require('../../../forms/Fieldset.react').Div;
 var Crumb = require('../../common/BreadCrumb.react').Crumb;
+var ActivityList = require('../ActivityList.react');
 
 function get_activity_number(user) {
     return _.size(ActivityStore.myFeed(user));
@@ -120,89 +121,6 @@ var FilterBar = React.createClass({
         }
     }
 
-});
-
-var ActivityListItem = React.createClass({
-    mixins: [AppContextMixin],
-    propTypes: {
-        activity: React.PropTypes.object,
-    },
-
-    getAuthor: function(user_id) {
-        return UserStore.get(user_id);
-    },
-
-    getContact: function(sc_id) {
-        return ContactStore.get(SalesCycleStore.get(sc_id).contact_id);
-    },
-
-    render: function() {
-        var activity = this.props.activity;
-        var author = this.getAuthor(activity.author_id);
-        var contact = this.getContact(activity.salescycle_id);
-        return (
-            <div className="stream-item">
-                <div className="row">
-                    <div className="row-icon">
-                        <IconSvg iconKey={activity.feedback} />
-                  </div>
-                  <div className="row-body row-body--no-trailer">
-                    <div className="row">
-                      <a href="#" className="row-icon">
-                        <figure className="icon-userpic">
-                            <img src={"img/userpics/" + author.userpic} />
-                        </figure>
-                      </a>
-                      <div className="row-body">
-                        <div className="row">
-                          <div className="row-body-primary text-caption text-secondary">
-                            <a href="#" className="text-secondary">{author.first_name} {author.last_name}</a> в {activity.at}
-                          </div>
-                          <div className="row-body-secondary">
-                            <a href="#" className="row-icon">
-                                <IconSvg iconKey="comment" />
-                            </a>
-                          </div>
-                        </div>
-                        <div className="row-body-message">
-                            {activity.description}
-                        </div>
-                        <ul className="stream-breadcrumbs">
-                            <li>
-                                <Link to='contact_profile' params={{id: contact.id}} className="stream-breadcrumbs">{contact.fn}</Link>
-                            </li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-        )
-    }
-});
-
-var ActivityList = React.createClass({
-    propTypes: {
-        filter_text: React.PropTypes.string,
-        activities: React.PropTypes.array,
-        selection_map: React.PropTypes.object,
-        onChangeState: React.PropTypes.func
-    },
-
-    render: function() {
-        var activityListItems = activities.map(function(activity) {
-            return(
-                <ActivityListItem
-                    activity={activity} />
-            )
-        }.bind(this));
-
-        return (
-            <div className="page-body">
-                {activityListItems}
-            </div>
-        )
-    }
 });
 
 var MyFeedDetailView = React.createClass({
