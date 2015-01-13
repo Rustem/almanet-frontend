7980afc9d795e2f21a14dfb926c553bc3a023d94
Sattar Stamkulov <sattar.coder@gmail.com>
2015-01-13 16:42:04 +0600

    fixed: SalesCycleStore products

--- js/stores/SalesCycleStore.js
+++ js/stores/SalesCycleStore.js
@@ -81,7 +81,7 @@ var SalesCycleStore = assign({}, EventEmitter.prototype, {
         _.forEach(obj.sales_cycles, function (sc){
             _salescycles[sc.id] = sc;
             _salescycles[sc.id].activities = [];
-            _salescycles[sc.id].product_ids = sc.product_ids;
+            _salescycles[sc.id].product_ids = obj.sales_cycles_to_products_map[sc.id];
         });
         _.forEach(obj.activities, function (actv){
             if(actv.salescycle_id in _salescycles)
